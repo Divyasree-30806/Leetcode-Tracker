6dc20cb393732a841dc562ac679787b73ebe950a
Divyasree-30806 <divyasreesenthil06@gmail.com>
2026-07-09 15:09:05 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/9/2026, 3:09:03 PM
+# Write your MySQL query statement below
+SELECT 
+    id,
+    CASE
+        WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id)
+        ELSE COALESCE(LEAD(student) OVER(ORDER BY id), student)
+    END AS student
+FROM Seat
