c25d9cc7176d6c3bee72bf51e6455fd85b319271
Divyasree-30806 <divyasreesenthil06@gmail.com>
2026-07-14 00:12:50 +0530

    Create 115-DistinctSubsequences

---
+++ 115-DistinctSubsequences/version/java/115-DistinctSubsequences_20260714_001243.java
@@ -0,0 +1,24 @@
+// Last updated: 7/14/2026, 12:12:43 AM
+1class Solution {
+2    public int numDistinct(String s, String t) {
+3        int n = s.length();
+4        int m = t.length();
+5
+6        int[][] dp = new int[n+1][m+1];
+7
+8        for(int i = 0; i <= n; i++){
+9            dp[i][0] = 1;
+10        }
+11
+12        for(int i = 1; i <= n; i++){
+13            for(int j = 1; j <= m; j++){
+14                if(s.charAt(i-1) == t.charAt(j-1)){
+15                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+16                }else{
+17                    dp[i][j] = dp[i-1][j];
+18                }
+19            }
+20        }
+21        return dp[n][m];
+22    }
+23}
